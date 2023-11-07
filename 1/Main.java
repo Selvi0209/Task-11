a74c956e41a2abd0c3cce107312d376d6849bf94
Karthick <selvi02091997@gmail.com>
2023-11-07 20:44:14 +0530

    What are the four access modifiers available in Java and what is their significance in terms of class, method, and variable accessibility?
    What are the four access modifiers available in Java and what is their significance in terms of class, method, and variable accessibility?

---
+++ 1/Main.java
@@ -0,0 +1,35 @@
+// Public class
+public class Main {
+
+    // Public method
+    public void publicMethod() {
+        System.out.println("This is a public method.");
+    }
+
+    // Protected method
+    protected void protectedMethod() {
+        System.out.println("This is a protected method.");
+    }
+
+    // Default method
+    void defaultMethod() {
+        System.out.println("This is a default method.");
+    }
+
+    // Private method
+    private void privateMethod() {
+        System.out.println("This is a private method.");
+    }
+
+    // Public variable
+    public int publicVariable = 10;
+
+    // Protected variable
+    protected int protectedVariable = 20;
+
+    // Default variable
+    int defaultVariable = 30;
+
+    // Private variable
+    private int privateVariable = 40;
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+<h1 align="center">
+  Task 11
+</h1> 
+
+* What are the four access modifiers available in Java and what is their significance in terms of class, method, and variable accessibility?
+  ## File Name : 1\Main.java
+  ## File Name : 1\Task_11_1_Description.txt
+  
+ 
